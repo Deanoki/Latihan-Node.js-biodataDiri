88d84fc1aab9d8ebc77a3a750270de3919a038ec
Dean Oki <75497106+Deanoki@users.noreply.github.com>
2023-05-09 12:46:09 +0700

    Latihan 3 Pesilat | membuat Biodata dengan Node.js

---
+++ node-js-moduleBiodata.js
@@ -0,0 +1,11 @@
+const biodata = {
+  nama: "Dean",
+  tempatLahir: "Bekasi",
+  tanggalLahir: "28/08/2000",
+  alamat: "Sumur Batu",
+};
+
+console.log("Nama :" + biodata.nama);
+console.log("Tempat Lahir :" + biodata.tempatLahir);
+console.log("Tanggal Lahir :" + biodata.tanggalLahir);
+console.log("Alamat :" + biodata.alamat);
